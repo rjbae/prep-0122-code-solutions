7396918723ce071c0069db02cb801248fd6c00e9
Ryan Bae <baeryan21@gmail.com>
2022-01-17 23:09:12 -0800

    Completed prep-javascript-arrays exercise.

---
+++ prep-javascript-arrays/main.js
@@ -0,0 +1,10 @@
+const languages = ['html', 'css', 'javascript', 'react', 'node', 'postgres'];
+console.log('languages array', languages);
+const firstElement = 'the ' + 'first ' + 'element ' + 'of ' + 'the ' + 'array ' + 'is ' + languages[0];
+console.log(firstElement);
+const thirdElement = 'the ' + 'third ' + 'element ' + 'of ' + 'the ' + 'array ' + 'is ' + languages[2];
+console.log(thirdElement);
+const length = 'The ' + 'length ' + 'of ' + 'the ' + 'array ' + 'is ' + languages.length;
+console.log(length);
+const lastElement = 'The ' + 'last ' + 'element ' + 'of ' + 'the ' + 'array ' + 'is ' + languages[languages.length - 1];
+console.log(lastElement);
