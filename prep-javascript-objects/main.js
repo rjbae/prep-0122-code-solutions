7efa37f206cf3f5f4c1d974f22c60c7951b7862f
Ryan Bae <baeryan21@gmail.com>
2022-01-17 21:56:42 -0800

    Completed prep-javascript-objects exercise.

--- prep-javascript-objects/main.js
+++ prep-javascript-objects/main.js
@@ -7,13 +7,13 @@ person.job = 'GOAT';
 person.previousJob = 'prodigy';
 console.log(person);
 
-const fullName = 'The ' + "person's " + 'name ' + 'is: ' + person.firstName + ' ' + person.lastName;
+const fullName = 'The ' + "person's " + 'name ' + 'is: ' + person.firstName + ' ' + person.lastName + '.';
 console.log(fullName);
 
-const job = 'The ' + "person's " + 'current ' + 'job ' + 'is: ' + person.job;
+const job = 'The ' + "person's " + 'current ' + 'job ' + 'is: ' + person.job + '.';
 console.log(job);
 
-const previousJob = 'The ' + "person's " + 'previous ' + 'job ' + 'is: ' + person.previousJob;
+const previousJob = 'The ' + "person's " + 'previous ' + 'job ' + 'is: ' + person.previousJob + '.';
 console.log(previousJob);
 
 console.log(person);
